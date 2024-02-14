47f4890cd48f17f7cefdbd74d9050b0f96b8bd19
Khusanjon <developerkhusanjon@gmail.com>
2024-02-14 21:12:42 +0500

    Initial commit

---
+++ src/main/java/com/fullstack/springbootreactfullstack/SpringBootReactFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.springbootreactfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactFullstackApplication.class, args);
+    }
+
+}
